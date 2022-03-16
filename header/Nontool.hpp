b19fd017db512f52c0b0f2c8c8225b62a4175a31
raniadf <16520006@mahasiswa.itb.ac.id>
2022-03-16 22:34:19 +0700

    add function

--- header/Nontool.hpp
+++ header/Nontool.hpp
@@ -8,13 +8,15 @@ using namespace std;
 class NonTool : public Item {
 protected :
 	int quantity;
+    string nttype;
 public :
 	NonTool();
     NonTool(int id, string name, string type, int quantity);
     ~NonTool();
+    int getQty() const;
+    string getNtType() const;
     void use();
 };
 //(derivation from nonTool -> Stone dll)
-
 
 #endif
