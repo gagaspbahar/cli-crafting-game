688689e16df11036361101dba2fd9bb1478f3492
Patrick Amadeus Irawan <patrickai0309@gmail.com>
2022-03-20 13:21:01 +0700

    Print CraftingTable

--- src/CraftingTable.cpp
+++ src/CraftingTable.cpp
@@ -13,12 +13,38 @@ Item& CraftingTable::getItem(int row, int col) {
 }
 
 void CraftingTable::printTable() {
-    // TODO: Search for pretty table libs
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << this->table[i][j].getId() << " ";
-        }
-        cout << endl;
-    }
+    // Get Max ItemName length
+	int mw = 0;
+	for(int i = 0 ; i < 3; i++){
+		for(int j = 0 ; j < 3 ;j++){
+			if (this->table[i][j].getName().length() > mw){
+				mw = this->table[i][j].getName().length();
+			}
+		}
+	}
+
+    //Print crafting table
+	for(int line = 0 ; line < mw*3 + 4 ; line++){
+		cout << "-";
+	}
+	cout << endl;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			int ws = this->table[i][j].getName().length();
+			cout << "|";
+			for(int space = 0 ; space < (mw - ws)/2 ; space++){
+				cout << " ";
+			} 
+			cout << this->table[i][j].getName();
+			for(int space = 0 ; space < (mw - ws)/2 + (ws%2 != 0); space++){
+				cout << " ";
+			} 
+		}
+		cout << "|" << endl;
+		for(int line = 0 ; line < mw*3+4 ; line++){
+			cout << "-";
+		}
+		cout << endl;
+	}
 }
 
