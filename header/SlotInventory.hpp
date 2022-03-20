c69f0a536776e708bb4b0eb40a2a995b19a094bf
Nelsen Putra <16520272@std.stei.itb.ac.id>
2022-03-21 05:08:44 +0700

    Add SlotInventory.hpp

---
+++ header/SlotInventory.hpp
@@ -0,0 +1,43 @@
+#ifndef _SLOTINVENTORY_HPP_
+#define _SLOTINVENTORY_HPP_
+
+#include <bits/stdc++.h>
+#include "Item.hpp"
+#include "Tool.hpp"
+#include "Nontool.hpp"
+using namespace std;
+
+class SlotInventory {
+    protected:
+        Item* slotContainer;
+        const int capacity = 64;
+        int quantity;
+        int slotId;
+    public:
+        SlotInventory();
+        SlotInventory(Item* slotContainer, int quantity, int slotId);
+        SlotInventory(const SlotInventory& si);
+        SlotInventory& operator=(const SlotInventory& si);
+        virtual ~SlotInventory();
+
+        // Getter
+        int getSlotId();
+        int getQuantity();
+        int getEmptyQuantity();
+        Item* getItemInfo();
+        string getNameFromSlotItem();
+        string getCategoryFromSlotItem();
+
+        // Setter
+        void setSlotId(int slotId);
+        void setQuantity(int currentQuantity);
+        
+        // Adder
+        void addQuantity(int quantity);
+        void addItemToSlot(Item* item, int quantity);
+
+        // Remover
+        void removeItem(int quantity);
+};
+
+#endif
